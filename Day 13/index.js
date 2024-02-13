301b781ecdface9b22fd111ddde0a6ec62817954
Kritika Gupta <83400697+Kritika30032002@users.noreply.github.com>
2024-02-13 18:13:19 +0530

    Completed Day 13

---
+++ Day 13/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const app = express();
+const server = http.createServer(app);
+function setupWebSocket(server) {
+  const wss = new WebSocket.Server({ server });
+
+  wss.on("connection", (ws) => {
+    console.log("Client connected");
+
+    ws.on("message", (message) => {
+      console.log(`Received message:${message}`);
+      ws.send(`Echo: ${message}`);
+    });
+
+    ws.on("close", () => {
+      console.log("Client disconnected");
+    });
+  });
+}
+
+setupWebSocket(server);
+
+app.get("/websocket", (req, res) => {
+  res.sendFile(__dirname + "/websocket.html");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
